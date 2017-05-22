1de238dc7bf8902f1d210df841eed6426b0928b5
Rob Skillington <r@uber.com>
2017-05-22 19:06:33 -0400

    Add results for BenchmarkMapLookupKeyStringFromBytes and BenchmarkMapSetKeyStringFromBytes

--- bench_map_key_lookup_string_from_bytes_test.go
+++ bench_map_key_lookup_string_from_bytes_test.go
@@ -1,5 +1,16 @@
 package main
 
+/*
+Results
+--
+$ go test -v -bench BenchmarkMap -benchmem
+testing: warning: no tests to run
+BenchmarkMapLookupKeyStringFromBytes-4          100000000               19.6 ns/op             0 B/op          0 allocs/op
+BenchmarkMapSetKeyStringFromBytes-4             20000000                73.8 ns/op             5 B/op          1 allocs/op
+PASS
+ok      github.com/robskillington/benchmarks-go 3.561s
+*/
+
 import (
 	"fmt"
 	"testing"
@@ -17,7 +28,6 @@ func BenchmarkMapLookupKeyStringFromBytes(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		// lookup[string(find)] = i
 		if _, ok := lookup[string(find)]; !ok {
 			b.Fatalf("key %s should exist", string(find))
 		}
